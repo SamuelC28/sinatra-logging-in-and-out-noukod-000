925ce48a5c2b956fde3ffb6d7f8178594241535b
Samuel Cornet <samuel@noukod.com>
2019-01-10 18:34:44 +0000

    Automatically backed up by Learn

--- db/migrate/20190110183315_create_user.rb
+++ db/migrate/20190110183315_create_user.rb
@@ -1,4 +1,8 @@
 class CreateUser < ActiveRecord::Migration[5.1]
   def change
+    create_table :users do |p|
+      t.string :username
+      t.string :password
+      t.decimal :balance
   end
 end
